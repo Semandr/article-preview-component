1c386e992b650d89860738772f8b065700d990ea
Semandr <semandr.code@gmail.com>
2022-06-09 20:52:26 +0200

    finished the card

--- js/app.js
+++ js/app.js
@@ -5,14 +5,22 @@ const formBlock2 = document.querySelector('.form__content-2');
 const formCheckBox = document.querySelector('.form__checkbox');
 
 formCheckBox.addEventListener('click', e => {
-  if (e.target.checked) {
-    formBlock1.classList.add('visually-hidden');
-    formBlock2.classList.remove('visually-hidden');
+  const width = window.innerWidth;
+  const target = e.target.checked;
+  if (target && width < 500) {
     formBlock.style.backgroundColor = 'hsl(217, 19%, 35%)';
+    formBlock1.classList.add('visually-hidden');
+    formBlock2.classList.remove('visually-hidden', 'popup');
   }
-  if (!e.target.checked) {
+  if ((!target && width < 500) || (!target && width > 500)) {
+    formBlock.style.backgroundColor = '#fff';
     formBlock1.classList.remove('visually-hidden');
     formBlock2.classList.add('visually-hidden');
-    formBlock.style.backgroundColor = '#fff';
+    formBlock2.classList.remove('popup');
+  }
+  if (target && width > 500) {
+    formBlock1.classList.remove('visually-hidden');
+    formBlock2.classList.remove('visually-hidden');
+    formBlock2.classList.add('popup');
   }
 });
